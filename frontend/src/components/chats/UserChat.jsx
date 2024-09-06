fe890384a75f9c6fa1c3d1a442fa0a2e2d8bd6ba
jaylyn36 <jaylyng02@icloud.com>
2024-09-06 16:10:04 -0400

    Added functionalities to UserChat

--- frontend/src/components/chats/UserChat.jsx
+++ frontend/src/components/chats/UserChat.jsx
@@ -1,12 +1,29 @@
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import { Stack } from "react-bootstrap";
 
 const UserChat = ({ chat, user }) => {
 
-    const { recipientUser } = useFetchRecipientUser(chat, user);
-    
-    console.log(recipientUser);
+    const { recipientUser } = useFetchRecipientUser(chat, user);    //custom hook
+
     return (
-        <>UserChat</>
+        <Stack direction = "horizontal" gap = {3} className = "user-card align-items-center p-2 justify-content-between">
+            <div className="d-flex">
+                <div className="me-2">
+                    A
+                </div>
+                <div className="text-content">
+                    <div className="name">{recipientUser?.name}</div>
+                    <div className="text">Message...</div>
+                </div>
+            </div>
+            <div className="d-flex flex-column align-items-end">
+                <div className="date">
+                    05/06/2024
+                </div>
+                <div className="this-user-notifications">2</div>
+                <span className="user-online"></span>
+            </div>
+        </Stack>
     );
 }
 
