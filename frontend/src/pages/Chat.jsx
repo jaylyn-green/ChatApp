cc2a8e7fafdd81d4fa4e3744c129014d22639102
jaylyn36 <jaylyng02@icloud.com>
2024-09-05 20:18:34 -0400

    Chat changes

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,13 +1,35 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap"
+import UserChat from "../components/chats/UserChat";
+import { AuthContext } from "../context/AuthContext";
 
 const Chat = () => {
     const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext);
-    
-    console.log("UserChats", userChats);
+    const { user } = useContext(AuthContext);
+
     return (
         <>
-            Chat page nothing yet...
+            <Container>
+                {
+                    userChats?.length < 1 ? null : (
+                        <Stack direction="horizontal" gap={4} className="align-items-start">
+                            <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+                                {isUserChatsLoading && <p>Loading...</p>}
+                                {userChats?.map((chat, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <UserChat chat={chat} user={user} />
+                                        </div>
+                                    )
+
+                                })}
+                            </Stack>
+                            <p>ChatBox</p>
+                        </Stack>
+                    )
+                }
+            </Container>
         </>
     );
 }
